53848d93092399bce102a727b647143f1493f555
esthercomputer <147074771+esthercomputer@users.noreply.github.com>
2023-10-05 18:36:49 +0400

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+int a, b; char c; int res=0;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two numbers");
+a = sc.nextInt();
+b = sc.nextInt();
+System.out.println("Enter an arithmetic operator");
+c = sc.next().charAt(0);
+switch (c)
+{
+    case '+':
+        res=a+b;
+    break;
+    case '-':
+        res=a-b;
+    break;
+    case '*':
+        res=a*b;
+    break;
+    case '/':
+        res=a/b;
+    break;
+    case '%':
+    res=a%b;
+    break;
+    default : 
+    System.out.println("Invalid");
+
+}
+System.out.println("The result is " +res);
+	
+	
+		
+		
+	}
+}
